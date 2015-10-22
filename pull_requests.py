01c7994d5b83bd1ee283fd140dbf404025faeae0
Matt Holland <matth@dnbcloud.com>
2015-10-22 11:24:46 -0700

    add comment count - closes #1

--- pull_requests.py
+++ pull_requests.py
@@ -9,25 +9,29 @@ headers = {
    'Authorization': 'token ' + config['token']
 }
 
+apiRoot = 'https://api.github.com/repos/'
+
 output = csv.writer(open(config['output'], 'wb'))
-output.writerow(['Repo', 'ID', 'Description', 'Created By', 'Open Date', 'Merged By', 'Merged Date'])
+output.writerow(['Repo', 'ID', 'Description', 'Created By', 'Open Date', 'Merged By', 'Merged Date', 'Comments'])
 
 for repo in config['repos']:
-   all = 'https://api.github.com/repos/' + repo + '/pulls?state=all'
-   individual = 'https://api.github.com/repos/' + repo + '/pulls/'
+   all = apiRoot + repo + '/pulls?state=all'
+   individual = apiRoot + repo + '/pulls/'
 
    request = requests.request('GET', all, headers=headers)
    parsed = request.json()
 
    for pull in parsed:
       details = requests.request('GET', individual + str(pull['number']), headers=headers).json()
+      comments = requests.request('GET', individual + str(pull['number']) + '/comments', headers=headers).json()
       output.writerow([repo,
                        pull['number'],
                        pull['title'].encode('ascii', 'ignore'),
                        pull['user']['login'],
                        pull['created_at'],
                        details['merged_by']['login'] if details['merged_by'] is not None else None,
-                       pull['merged_at']])
+                       pull['merged_at'],
+                       len(comments)])
 
 print(str(request.headers['X-RateLimit-Remaining']) + ' requests remaining out of ' +
       str(request.headers['X-RateLimit-Limit']))
